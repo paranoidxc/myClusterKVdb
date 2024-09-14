dc7552a8a99216a80c81d5bdc90a16652987a9c8
paranoidxc <paranoid.xc@gmail.com>
2024-09-14 17:19:57 +0800

    echo

--- lib/logger/logger.go
+++ lib/logger/logger.go
@@ -1,7 +1,99 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"path/filepath"
+	"runtime"
+)
 
-func Info(s string) {
-	fmt.Println(s)
+type Level int
+
+type Settings struct {
+	Path       string
+	Name       string
+	Ext        string
+	TimeFORMAT string
+}
+
+var (
+	DefaultPrefix      = ""
+	DefaultCallerDepth = 2
+
+	logger     *log.Logger
+	isDebug    = false
+	levelFlags = []string{"DEBUG", "INFO", "WARN", "ERROR", "FATAL"}
+)
+
+const (
+	DEBUG Level = iota
+	INFO
+	WARNING
+	ERROR
+	FATAL
+)
+
+// Setup initialize the log instance
+func Setup(setting *Settings) {
+	logger = log.New(os.Stdout, DefaultPrefix, log.Ldate|log.Ltime|log.Lmicroseconds|log.Lmsgprefix)
+	file, err := os.OpenFile(fmt.Sprintf("%s/%s.%s", setting.Path, setting.Name, setting.Ext), os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	writers := []io.Writer{
+		file,
+		os.Stdout}
+	fileAndStdoutWriter := io.MultiWriter(writers...)
+	if err == nil {
+		logger.SetOutput(fileAndStdoutWriter)
+	}
+}
+
+func SetDebugMode(debug bool) {
+	isDebug = debug
+}
+
+// Debug output logs at debug level
+func Debug(v ...interface{}) {
+	if !isDebug {
+		return
+	}
+	setPrefix(DEBUG)
+	logger.Println(v...)
+}
+
+// Info output logs at info level
+func Info(v ...interface{}) {
+	setPrefix(INFO)
+	logger.Println(v...)
+}
+
+// Warn output logs at warn level
+func Warn(v ...interface{}) {
+	setPrefix(WARNING)
+	logger.Println(v...)
+}
+
+// Error output logs at error level
+func Error(v ...interface{}) {
+	setPrefix(ERROR)
+	logger.Println(v...)
+}
+
+// Fatal output logs at fatal level
+func Fatal(v ...interface{}) {
+	setPrefix(FATAL)
+	logger.Fatalln(v...)
+}
+
+// setPrefix set the prefix of the log output
+func setPrefix(level Level) {
+	var logPrefix string
+	_, file, line, ok := runtime.Caller(DefaultCallerDepth)
+	if ok {
+		logPrefix = fmt.Sprintf("%s - %s:%d ", levelFlags[level], filepath.Base(file), line)
+	} else {
+		logPrefix = fmt.Sprintf("%s ", levelFlags[level])
+	}
+
+	logger.SetPrefix(logPrefix)
 }
---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"myredis/config"
+	"myredis/lib/logger"
+	"myredis/tcp"
+	"os"
+)
+
+const configFile string = "redis.conf"
+
+var defaultProperties = &config.ServerProperties{
+	Bind: "0.0.0.0",
+	Port: 6379,
+}
+
+func fileExists(fileName string) bool {
+	info, err := os.Stat(fileName)
+	return err == nil && !info.IsDir()
+}
+
+func main() {
+	logger.Setup(&logger.Settings{
+		Path:       "logs",
+		Name:       "myredis",
+		Ext:        "log",
+		TimeFORMAT: "2006-01-02",
+	})
+	logger.SetDebugMode(true)
+
+	if fileExists(configFile) {
+		//config.SetupConfig(configFile)
+	} else {
+	}
+	config.Properties = defaultProperties
+
+	err := tcp.ListenAndServeWithSignal(&tcp.Config{
+		Address: fmt.Sprintf("%s:%d", config.Properties.Bind, config.Properties.Port),
+	}, tcp.MakeHandler())
+
+	if err != nil {
+		fmt.Println(err)
+		logger.Error(err)
+	}
+}
